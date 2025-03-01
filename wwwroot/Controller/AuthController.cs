43fca2eca7a8bf7449e01dbbcc5c6326b550d67d
Jobirmizo <hishimovanvar@gmail.com>
2025-03-01 17:23:55 +0500

    Removed appsettings.json from Git tracking

---
+++ wwwroot/Controller/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityKitchen.Features.Auth;
+
+namespace UniversityKitchen.wwwroot.Controller;
+
+public class AuthController : Microsoft.AspNetCore.Mvc.Controller
+{
+
+    public IActionResult Login()
+    {
+        return View(new LoginView());
+    }
+}
